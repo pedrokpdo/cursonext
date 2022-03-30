7b0d9b3cfcee1bd53794f467067188bfcd93682d
pedrokpdo <pedromotoehm@gmail.com>
2022-03-30 11:26:42 -0300

    parametizaçao nas rotas

--- src/pages/api/users/[...params].tsx
+++ src/pages/api/users/[...params].tsx
@@ -1,6 +1,8 @@
 import {NextApiRequest , NextApiResponse} from 'next'
 
 export default (request : NextApiRequest, response:NextApiResponse) => {
+   const id = request.query
+   
     const users = [
         {id:1, name:'pedro'},
         {id:2, name:'paula'},
