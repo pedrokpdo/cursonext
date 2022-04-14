0e7e3a90a6820997903e8dddeeb04e2a47d330f6
pedrokpdo <pedromotoehm@gmail.com>
2022-04-14 10:01:06 -0300

    teste do signInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -24,7 +24,7 @@ export function SignInButton() {
             onClick= {()=> signIn('github')}
         >
             <FaGithub color='#eba417' />
-            Sign with github
+            Sign in with Github
         </button>
     )
 
